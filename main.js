02a70ae37f744ef4e13e5f5b8d682abcf16102f0
coder-sam-007 <sameerjain.coder@gmail.com>
2023-10-25 22:41:31 +0530

    service section changed

---
+++ main.js
@@ -0,0 +1,24 @@
+const revealButton = document.querySelector(".service-item:nth-child(2) .btn");
+const containerWrapper = document.querySelector(".container-wrapper");
+
+revealButton.addEventListener("click", () => {
+  const currentScrollPosition = containerWrapper.scrollLeft;
+
+  if (currentScrollPosition === 0) {
+    const scrollPosition =
+      containerWrapper.scrollWidth - containerWrapper.clientWidth;
+
+    revealButton.style.rotate = "180deg";
+
+    containerWrapper.scrollTo({
+      left: scrollPosition,
+      behavior: "smooth",
+    });
+  } else {
+    revealButton.style.rotate = "0deg";
+    containerWrapper.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+});
