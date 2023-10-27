2eb464083c8d0156ba29718ba76c488c6e2d9440
coder-sam-007 <sameerjain.coder@gmail.com>
2023-10-27 22:21:33 +0530

    animation testing

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const header = document.querySelector("header");
 const hambugerMenu = document.querySelector(".hamburger");
+const revealButton = document.querySelector(".service-item:nth-child(2) .btn");
+const containerWrapper = document.querySelector(".container-wrapper");
+const animatedElements = document.querySelectorAll(".animate");
 
 hambugerMenu.addEventListener("click", () => {
   header.classList.toggle("active");
 });
-
-const revealButton = document.querySelector(".service-item:nth-child(2) .btn");
-const containerWrapper = document.querySelector(".container-wrapper");
 
 revealButton.addEventListener("click", () => {
   const currentScrollPosition = containerWrapper.scrollLeft;
@@ -28,4 +28,19 @@ revealButton.addEventListener("click", () => {
       behavior: "smooth",
     });
   }
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+animatedElements.forEach((el) => {
+  observer.observe(el);
 });
